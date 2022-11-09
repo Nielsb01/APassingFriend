d248a2cba9960dae83295908916e3e76a17e0594
Nielsb01 <nielsb1101@gmail.com>
2022-11-09 15:39:36 +0100

    Add jump to character movement
    -create feature folder in script
    -add jump mechanic to player movement

--- A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
+++ A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 using static UnityEngine.GraphicsBuffer;
 
 public class CharacterMovementScript : MonoBehaviour
@@ -10,19 +12,26 @@ public class CharacterMovementScript : MonoBehaviour
     [SerializeField] private float acceleration = 0.8f;
     [SerializeField] private float deceleration = 1.6f;
     [SerializeField] private float moveSpeed = 1.75f;
+    [SerializeField] private float jumpSpeed = 4.5f;
+    [SerializeField] private float gravity = 9.81f;
 
     private CharacterController characterController;
+    private new Rigidbody rigidbody;
 
     private float velocityY = 0.0f;
     private float velocityX = 0.0f;
     private float maxPositiveVelocity = 2.0f;
     private float maxNegativeVelocity = -2.0f;
-
+    
     private Vector2 moveVector;
+    private Vector3 moveDirection = Vector3.zero;
+    private bool doJump;
 
     void Start()
     {
+        doJump = false;
         characterController = GetComponent<CharacterController>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -32,6 +41,12 @@ public class CharacterMovementScript : MonoBehaviour
 
     private void Move()
     {
+        if (doJump)
+        {
+            moveDirection.y = jumpSpeed;
+            doJump = false;
+        }
+
         if (moveVector != null)
         {
             if (velocityY < maxPositiveVelocity && moveVector.y > 0)
@@ -78,10 +93,12 @@ public class CharacterMovementScript : MonoBehaviour
                 }
             }
 
-            velocityX = (float)Math.Round(velocityX, 4);
-            velocityY = (float)Math.Round(velocityY, 4);
-            //Debug.Log("Vx: " + velocityX + " vY: " + velocityY);
-            characterController.Move(transform.TransformDirection(new Vector3(moveSpeed * velocityX * Time.deltaTime, 0, moveSpeed * velocityY * Time.deltaTime)));
+            //Debug.Log("Vx: " + (float)Math.Round(velocityX, 4) + " vY: " + (float)Math.Round(velocityY, 4));
+            moveDirection.x = moveSpeed * ((float)Math.Round(velocityX, 4));
+            moveDirection.z = moveSpeed * ((float)Math.Round(velocityY, 4));
+
+            moveDirection.y -= gravity * Time.deltaTime;
+            characterController.Move(transform.TransformDirection(moveDirection * Time.deltaTime));
         }
     }
 
@@ -94,5 +111,14 @@ public class CharacterMovementScript : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         moveVector = inputValue.Get<Vector2>();
+    }
+
+    private void OnJump()
+    {
+        Debug.Log("press jump");
+        if (characterController.isGrounded)
+        {
+            doJump = true;
+        }
     }
 }
