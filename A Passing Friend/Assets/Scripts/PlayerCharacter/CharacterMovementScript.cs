68dd2c4e0fbd4948a9dc34fc6df2dd98e0322328
Nielsb01 <nielsb1101@gmail.com>
2022-11-10 14:32:42 +0100

    Update CharacterMovementScript.cs
    Review comments resolved

--- A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
+++ A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
@@ -4,29 +4,29 @@ using UnityEngine.InputSystem;
 
 public class CharacterMovementScript : MonoBehaviour
 {
-    [SerializeField] private float acceleration = 0.8f;
-    [SerializeField] private float deceleration = 1.6f;
-    [SerializeField] private float moveSpeed = 1.75f;
-    [SerializeField] private float jumpSpeed = 4.5f;
-    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float _acceleration = 0.8f;
+    [SerializeField] private float _deceleration = 1.6f;
+    [SerializeField] private float _moveSpeed = 1.75f;
+    [SerializeField] private float _jumpSpeed = 4.5f;
+    [SerializeField] private float _gravity = 9.81f;
 
     private CharacterController characterController;
-    private new Rigidbody rigidbody;
 
-    private float velocityY = 0.0f;
-    private float velocityX = 0.0f;
-    private float maxPositiveVelocity = 2.0f;
-    private float maxNegativeVelocity = -2.0f;
+    private float _velocityY = 0.0f;
+    private float _velocityX = 0.0f;
+    private float _maxPositiveVelocity = 2.0f;
+    private float _maxNegativeVelocity = -2.0f;
 
-    private Vector2 moveVector;
-    private Vector3 moveDirection = Vector3.zero;
-    private bool doJump;
+    private Vector2 _moveVector;
+    private Vector3 _moveDirection = Vector3.zero;
+    private bool _doJump;
+
+    private float _checkValue = 0.01f;
 
     void Start()
     {
-        doJump = false;
+        _doJump = false;
         characterController = GetComponent<CharacterController>();
-        rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -36,64 +36,66 @@ public class CharacterMovementScript : MonoBehaviour
 
     private void Move()
     {
-        if (doJump)
+        if (_moveVector == null)
         {
-            moveDirection.y = jumpSpeed;
-            doJump = false;
+            return;
         }
 
-        if (moveVector != null)
+        if (_doJump)
         {
-            if (velocityY < maxPositiveVelocity && moveVector.y > 0)
-            {
-                velocityY += Time.deltaTime * acceleration;
-            }
-            else if (velocityY > 0)
-            {
-                velocityY -= Time.deltaTime * deceleration;
-            }
-
-            if (velocityY > maxNegativeVelocity && moveVector.y < 0)
-            {
-                velocityY -= Time.deltaTime * acceleration;
-            }
-            else if (velocityY < 0)
-            {
-                velocityY += Time.deltaTime * deceleration;
-                if (floatIsBetween(velocityY, 0, 0.01f))
-                {
-                    velocityY = 0;
-                }
-            }
-
-            if (velocityX < maxPositiveVelocity && moveVector.x > 0)
-            {
-                velocityX += Time.deltaTime * acceleration;
-            }
-            else if (velocityX > 0)
-            {
-                velocityX -= Time.deltaTime * deceleration;
-            }
-
-            if (velocityX > maxNegativeVelocity && moveVector.x < 0)
-            {
-                velocityX -= Time.deltaTime * acceleration;
-            }
-            else if (velocityX < 0)
-            {
-                velocityX += Time.deltaTime * deceleration;
-                if (floatIsBetween(velocityX, 0, 0.01f))
-                {
-                    velocityX = 0;
-                }
-            }
-
-            moveDirection.x = moveSpeed * ((float)Math.Round(velocityX, 4));
-            moveDirection.z = moveSpeed * ((float)Math.Round(velocityY, 4));
-
-            moveDirection.y -= gravity * Time.deltaTime;
-            characterController.Move(transform.TransformDirection(moveDirection * Time.deltaTime));
+            _moveDirection.y = _jumpSpeed;
+            _doJump = false;
         }
+
+        if (_velocityY < _maxPositiveVelocity && _moveVector.y > 0)
+        {
+            _velocityY += Time.deltaTime * _acceleration;
+        }
+        else if (_velocityY > 0)
+        {
+            _velocityY -= Time.deltaTime * _deceleration;
+        }
+
+        if (_velocityY > _maxNegativeVelocity && _moveVector.y < 0)
+        {
+            _velocityY -= Time.deltaTime * _acceleration;
+        }
+        else if (_velocityY < 0)
+        {
+            _velocityY += Time.deltaTime * _deceleration;
+            if (floatIsBetween(_velocityY, 0, _checkValue))
+            {
+                _velocityY = 0;
+            }
+        }
+
+        if (_velocityX < _maxPositiveVelocity && _moveVector.x > 0)
+        {
+            _velocityX += Time.deltaTime * _acceleration;
+        }
+        else if (_velocityX > 0)
+        {
+            _velocityX -= Time.deltaTime * _deceleration;
+        }
+
+        if (_velocityX > _maxNegativeVelocity && _moveVector.x < 0)
+        {
+            _velocityX -= Time.deltaTime * _acceleration;
+        }
+        else if (_velocityX < 0)
+        {
+            _velocityX += Time.deltaTime * _deceleration;
+            if (floatIsBetween(_velocityX, 0, _checkValue))
+            {
+                _velocityX = 0;
+            }
+        }
+
+        _moveDirection.x = _moveSpeed * ((float)Math.Round(_velocityX, 4));
+        _moveDirection.z = _moveSpeed * ((float)Math.Round(_velocityY, 4));
+
+        _moveDirection.y -= _gravity * Time.deltaTime;
+        characterController.Move(transform.TransformDirection(_moveDirection * Time.deltaTime));
     }
 
     private static bool floatIsBetween(float number, float min, float max)
@@ -104,14 +106,14 @@ public class CharacterMovementScript : MonoBehaviour
 
     private void OnMove(InputValue inputValue)
     {
-        moveVector = inputValue.Get<Vector2>();
+        _moveVector = inputValue.Get<Vector2>();
     }
 
     private void OnJump()
     {
         if (characterController.isGrounded)
         {
-            doJump = true;
+            _doJump = true;
         }
     }
 }
