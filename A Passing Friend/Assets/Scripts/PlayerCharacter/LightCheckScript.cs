7803a037ff7db645ea863c14b0cc343fc65141d5
DannyHageman <48557943+DannyHageman@users.noreply.github.com>
2022-11-14 14:24:41 +0100

    Added optimalization, changed foldernames & integrated with player movement
    Optimalization changes
     - 1 less camera, and made all of them angled
     - LightCheckScript -> changed the tmp2Dtexture into a variable

--- A Passing Friend/Assets/Scripts/PlayerCharacter/LightCheckScript.cs
+++ A Passing Friend/Assets/Scripts/PlayerCharacter/LightCheckScript.cs
@@ -14,17 +14,19 @@ public class LightCheckScript : MonoBehaviour
     [HideInInspector] public int lightLevel;
     public bool calculateLight;
     [SerializeField] private bool _dayTime = true;
-    [SerializeField] private RenderTexture _topLightCheckTexture;
     [SerializeField] private RenderTexture _leftLightCheckTexture;
     [SerializeField] private RenderTexture _rightLightCheckTexture;
-    [SerializeField] private RenderTexture _forwardLightCheckTexture;
     [SerializeField] private RenderTexture _backwardLightCheckTexture;
+    [SerializeField] private RenderTexture _forwardLightCheckTexture;
 
     private int _initialLightlevel;
+    private Texture2D _tmp2DTexture;
 
     private void Awake()
     {
         _initialLightlevel = _dayTime ? INITIAL_LIGHTLEVEL_DAYTIME : INITIAL_LIGHTLEVEL_NIGHTTIME;
+        // All renderTextures have to be of the same size
+        _tmp2DTexture = new(_leftLightCheckTexture.width, _leftLightCheckTexture.height);
     }
 
     private void Update()
@@ -33,7 +35,6 @@ public class LightCheckScript : MonoBehaviour
 
         var lightLevels = new float[] 
         {
-            GetPixelsFromTexture(_topLightCheckTexture),
             GetPixelsFromTexture(_leftLightCheckTexture),
             GetPixelsFromTexture(_rightLightCheckTexture),
             GetPixelsFromTexture(_forwardLightCheckTexture),
@@ -53,15 +54,13 @@ public class LightCheckScript : MonoBehaviour
         var previous = RenderTexture.active;
         RenderTexture.active = tmpTexture;
 
-        Texture2D tmp2DTexture = new(lightCheckTexture.width, lightCheckTexture.height);
-        tmp2DTexture.ReadPixels(new Rect(0, 0, tmpTexture.width, tmpTexture.height), 0, 0);
-        tmp2DTexture.Apply();
+        _tmp2DTexture.ReadPixels(new Rect(0, 0, tmpTexture.width, tmpTexture.height), 0, 0);
+        _tmp2DTexture.Apply();
 
         RenderTexture.active = previous;
         RenderTexture.ReleaseTemporary(tmpTexture);
 
-        var pixels = tmp2DTexture.GetPixels32();
-        Destroy(tmp2DTexture);
+        var pixels = _tmp2DTexture.GetPixels32();
 
         // Wikipedia contributors. (2021, 3 november). Relative luminance. Wikipedia. https://en.wikipedia.org/wiki/Relative_luminance#:~:text=Relative%20luminance%20and%20%22gamma%20encoded%22%20colorspaces%5Bedit%5D
         var totalLuminance = pixels.Sum(pixel => 0.2126f * pixel.r + 0.7152f * pixel.g + 0.0722f * pixel.b);
