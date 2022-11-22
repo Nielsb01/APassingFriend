8efe70031690aa28117be5c88d6c86363f0ee01d
HugovdWel <HAB.vanderWel@student.han.nl>
2022-11-22 15:45:12 +0100

    added test track and waypoint speedchange work in progress

--- A Passing Friend/Assets/Scripts/Npc/NpcMovementController.cs
+++ A Passing Friend/Assets/Scripts/Npc/NpcMovementController.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,21 +17,26 @@ namespace Assets.Scripts.Npc
         [SerializeField] private bool _patrolling = false;
         [SerializeField] private GameObject _pathNodePrefab;
         private NavMeshAgent _navMeshAgent;
+        private VariableDeclarations _variables;
+        private float _waypointRoundingForNextNode;
 
         private void Start()
         {
+            _variables = GetComponent<Variables>().declarations;
             _navMeshAgent = GetComponent<NavMeshAgent>();
             if (_waypointRounding < 0.01f)
             {
                 _waypointRounding = 0.01f;
             }
+
+            _waypointRoundingForNextNode = _waypointRounding;
         }
 
         private void Update()
         {
             var destination = new Vector2(_navMeshAgent.destination.x, _navMeshAgent.destination.z);
             var currentPos = new Vector2(transform.position.x, transform.position.z);
-            if ((destination - currentPos).magnitude < _waypointRounding && _waypoints.Count > 0)
+            if ((destination - currentPos).magnitude < _waypointRoundingForNextNode && _waypoints.Count > 0)
             {
                 if (_patrolling)
                 {
@@ -76,6 +85,7 @@ namespace Assets.Scripts.Npc
             if (_waypoints.Count > 0)
             {
                 _navMeshAgent.destination = _waypoints.First().transform.position;
+                CheckIfNextNodeHasEffect(_waypoints.First());
             }
         }
 
@@ -84,13 +94,61 @@ namespace Assets.Scripts.Npc
             return Instantiate(_pathNodePrefab, vector3, Quaternion.identity);
         }
 
-        private void CheckIfNodeHasEffect(GameObject node)
+        private void CheckIfNextNodeHasEffect(GameObject node)
         {
-            if(node.GetComponent<Variables>())
+            Debug.Log(1);
+            Debug.Log(_variables);
+            Debug.Log(2);
+            Debug.Log(_variables.Get("RoundingForThisNode"));
+            Debug.Log(3);
+            Debug.Log((float)_variables.Get("RoundingForThisNode"));
+            ApplyMovementSpeedChange();
+
+            var roundingForThisNode = (float)_variables.Get("RoundingForThisNode");
+            SetRoundingForNextWaypoint(roundingForThisNode > 0, roundingForThisNode);
+
+            var waitTimeAtThisNode = (float)_variables.Get("WaitTimeAtThisNode");
+            if (waitTimeAtThisNode > 0)
+            {
+                ApplyWaitTimeAtThisNode(waitTimeAtThisNode);
+            }
+
+            var triggerEvent = (bool)_variables.Get("TriggerEvent");
+            if (triggerEvent)
+            {
+                ApplyTriggerEvent();
+            }
         }
 
-        private void ApplyNodeEffect()
+        private void SetRoundingForNextWaypoint(bool customRounding, float rounding)
         {
+            _waypointRoundingForNextNode = customRounding ? rounding : _waypointRounding;
+        }
+
+        private void ApplyMovementSpeedChange()
+        {
+            var newMovementSpeed = (float)_variables.Get("NewMovementSpeed");
+            if (newMovementSpeed > 0)
+            {
+                _navMeshAgent.speed = newMovementSpeed;
+            }
+        }
+
+        private void ApplyWaitTimeAtThisNode(float time)
+        {
+            StartCoroutine(WaitForTimeAtNode(time));
+        }
+
+        private void ApplyTriggerEvent()
+        {
+        }
+
+        private IEnumerator WaitForTimeAtNode(float time)
+        {
+            var speed = _navMeshAgent.speed;
+            _navMeshAgent.speed = 0;
+            yield return new WaitForSeconds(time);
+            _navMeshAgent.speed = speed;
         }
     }
 }
