4780aec2ce27aa7d15bbd0791b547b4d3e5330d3
HugovdWel <HAB.vanderWel@student.han.nl>
2022-12-05 10:54:07 +0100

    added f to lock cursor and fixed build not hiding path nodes

--- A Passing Friend/Assets/Scripts/DebugToolkit/ShowObjectOnlyInEditor.cs
+++ A Passing Friend/Assets/Scripts/DebugToolkit/ShowObjectOnlyInEditor.cs
@@ -1,24 +1,15 @@
+using System;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 namespace DebugToolkit
 {
-    [ExecuteInEditMode]
     public class ShowObjectOnlyInEditor : MonoBehaviour
     {
-        private Renderer _renderer;
-
-        public void OnValidate()
+        private void Start()
         {
-            _renderer = GetComponent<Renderer>();
-            if (!EditorApplication.isPlaying && !Application.isPlaying) return;
-            HideIfGameIsRunning();
-        }
-
-        private void HideIfGameIsRunning()
-        {
-            _renderer.enabled = false;
+            GetComponent<Renderer>().enabled = false;
             var lightComp = GetComponent<Light>();
             if (lightComp != null)
             {
