7274a68a9e0ea59f93666f982d7c98e1254147c9
Nathan Wijnberg <57752436+Nwijnberg@users.noreply.github.com>
2022-12-08 13:28:05 +0100

    Revert "Merge branch 'develop' into sub-task/feature-6-Charge-jump-bar"
    This reverts commit d4e4745da60dff26d9f0ef07bf8906ab385327c1, reversing
    changes made to b8759ce367e51720f8edef60b458c8dc1eb35044.

--- A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
+++ A Passing Friend/Assets/Scripts/PlayerCharacter/CharacterMovementScript.cs
@@ -90,11 +90,6 @@ public class CharacterMovementScript : MonoBehaviour, IDataPersistence
         {
             _jumpCharged += _chargeSpeed * Time.deltaTime;
         }
-
-        if (!_characterController.isGrounded && _jumpCharged > 0)
-        {
-            _jumpCharged = 0;
-        }
     }
 
     private void Move()
@@ -248,7 +243,7 @@ public class CharacterMovementScript : MonoBehaviour, IDataPersistence
 
     private void OnJumpHold()
     {
-        if (_chargeJumpUnlocked && _characterController.isGrounded)
+        if (_chargeJumpUnlocked)
         { 
             _holdingDownJump = true;
         }
